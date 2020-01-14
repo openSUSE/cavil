511254118241d00f4fdabfc8e07461f2dc7a38d4
Stephan Kulow <coolo@suse.de>
2020-01-14 15:58:42 +0100

    Show the dropdown on hover

--- assets/app.js
+++ assets/app.js
@@ -539,6 +539,13 @@ function setupReviewDetails(url) {
         location.reload();
       });
     });
+
+    $('.dropdown').hover(
+      function() {
+        $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(100);
+      }, function() {
+        $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(100);
+      });
   });
 
   $('#create-pattern-and-continue').click(createPatternAndContinue);
