ccc7cb70ed1bbec057bef7d2b2c4f4c58fddcb80
Sebastian Riedel <sri@cpan.org>
2021-04-27 13:32:08 +0200

    Fix broken tooltip

--- assets/app.js
+++ assets/app.js
@@ -25,11 +25,9 @@ function backToTop () {
       }
     });
     $('#back-to-top').click(function () {
-      $('#back-to-top').tooltip('hide');
       $('body, html').animate({scrollTop: 0}, 800);
       return false;
     });
-    $('#back-to-top').tooltip('show');
   });
 }
 
