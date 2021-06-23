a6da0ae2b70aa0e1005f04ce024321b382d64d6b
Sebastian Riedel <sri@cpan.org>
2021-06-23 18:10:38 +0200

    Convert Cavil::Model::Packages to signatures

--- lib/Cavil/Model/Packages.pm
+++ lib/Cavil/Model/Packages.pm
@@ -14,15 +14,14 @@
 # with this program; if not, see <http://www.gnu.org/licenses/>.
 
 package Cavil::Model::Packages;
-use Mojo::Base -base;
+use Mojo::Base -base, -signatures;
 
 use Mojo::File 'path';
 use Mojo::Util 'dumper';
 
 has [qw(checkout_dir minion pg)];
 
-sub add {
-  my ($self, %args) = @_;
+sub add ($self, %args) {
 
   my $db = $self->pg->db;
   my $source
@@ -41,8 +40,7 @@ sub add {
   return $db->insert('bot_packages', $pkg, {returning => 'id'})->hash->{id};
 }
 
-sub actions {
-  my ($self, $link, $id) = @_;
+sub actions ($self, $link, $id) {
 
   # Requests with multiple actions are an OBS thing
   return [] if !$link || !($link =~ /^obs#/ || $link =~ /^ibs#/);
@@ -56,15 +54,13 @@ sub actions {
   )->hashes->to_array;
 }
 
-sub all { shift->pg->db->select('bot_packages')->hashes }
+sub all ($self) { $self->pg->db->select('bot_packages')->hashes }
 
-sub analyze {
-  my ($self, $id, $priority, $parents) = (shift, shift, shift // 9, shift || []);
+sub analyze ($self, $id, $priority = 9, $parents = []) {
   return $self->_enqueue('analyze', $id, $priority, $parents);
 }
 
-sub find {
-  my ($self, $id) = @_;
+sub find ($self, $id) {
   return $self->pg->db->select(
     ['bot_packages', [-left => 'bot_users', id => 'reviewing_user']],
     [
@@ -77,8 +73,7 @@ sub find {
   )->hash;
 }
 
-sub find_by_name {
-  my ($self, $name) = @_;
+sub find_by_name ($self, $name) {
   return $self->pg->db->select(
     ['bot_packages', [-left => 'bot_users', id => 'reviewing_user']],
     [
@@ -91,13 +86,11 @@ sub find_by_name {
   )->hashes;
 }
 
-sub find_by_name_and_md5 {
-  my ($self, $pkg, $md5) = @_;
+sub find_by_name_and_md5 ($self, $pkg, $md5) {
   return $self->pg->db->select('bot_packages', '*', {name => $pkg, checkout_dir => $md5})->hash;
 }
 
-sub history {
-  my ($self, $name, $checksum, $id) = @_;
+sub history ($self, $name, $checksum, $id) {
   return $self->pg->db->query(
     "select p.id, external_link, result, state, login,
        extract(epoch from created) as created_epoch, obsolete
@@ -107,8 +100,7 @@ sub history {
   )->hashes->to_array;
 }
 
-sub ignore_line {
-  my ($self, $package, $hash) = @_;
+sub ignore_line ($self, $package, $hash) {
 
   my $db = $self->pg->db;
   $db->query(
@@ -123,24 +115,21 @@ sub ignore_line {
   }
 }
 
-sub imported {
-  my ($self, $id) = @_;
+sub imported ($self, $id) {
   $self->pg->db->update('bot_packages', {imported => \'now()'}, {id => $id});
 }
 
-sub index { shift->_enqueue('index', @_) }
+sub index ($self, @args) { $self->_enqueue('index', @args) }
 
-sub indexed {
-  my ($self, $id) = @_;
+sub indexed ($self, $id) {
   $self->pg->db->update('bot_packages', {indexed => \'now()'}, {id => $id});
 }
 
-sub is_imported { shift->_check_timestamp('imported', @_) }
-sub is_indexed  { shift->_check_timestamp('indexed',  @_) }
-sub is_unpacked { shift->_check_timestamp('unpacked', @_) }
+sub is_imported ($self, @args) { shift->_check_timestamp('imported', @args) }
+sub is_indexed  ($self, @args) { shift->_check_timestamp('indexed',  @args) }
+sub is_unpacked ($self, @args) { shift->_check_timestamp('unpacked', @args) }
 
-sub list {
-  my ($self, $state, $pkg) = @_;
+sub list ($self, $state, $pkg) {
 
   # Do not show obsolete packages
   my %where;
@@ -154,13 +143,11 @@ sub list {
     ->hashes->to_array;
 }
 
-sub mark_matched_for_reindex {
-  my ($self, $pid, $priority) = (shift, shift, shift // 0);
+sub mark_matched_for_reindex ($self, $pid, $priority = 0) {
   $self->minion->enqueue(reindex_matched_later => [$pid] => {priority => $priority});
 }
 
-sub name_suggestions {
-  my ($self, $partial) = @_;
+sub name_suggestions ($self, $partial) {
   my $like = '%' . $partial . '%';
   return $self->pg->db->select(
     'bot_packages', [\'distinct(name)'],
@@ -169,16 +156,13 @@ sub name_suggestions {
   )->arrays->flatten->to_array;
 }
 
-sub obs_import {
-  my ($self, $id, $data, $priority) = (shift, shift, shift, shift // 5);
+sub obs_import ($self, $id, $data, $priority = 5) {
   my $pkg = $self->find($id);
   return $self->minion->enqueue(obs_import => [$id, $data] =>
       {priority => $priority, notes => {external_link => $pkg->{external_link}, package => $pkg->{name}}});
 }
 
-sub obsolete_if_not_in_product {
-  my ($self, $id) = @_;
-
+sub obsolete_if_not_in_product ($self, $id) {
   my $db = $self->pg->db;
   return undef if $db->query('select 1 from bot_package_products where package = ? limit 1', $id)->array;
   $db->query(
@@ -188,8 +172,7 @@ sub obsolete_if_not_in_product {
   return 1;
 }
 
-sub recent {
-  my $self = shift;
+sub recent ($self) {
   return $self->pg->db->query(
     'select p.*, u.login, extract(epoch from p.created) as created_epoch,
        extract(epoch from p.reviewed) as reviewed_epoch
@@ -201,8 +184,7 @@ sub recent {
   )->hashes->to_array;
 }
 
-sub reindex {
-  my ($self, $id, $priority) = @_;
+sub reindex ($self, $id, @args) {
 
   # Protect from race conditions (even before creating a background job)
   return undef unless $self->minion->lock("processing_pkg_$id", 0);
@@ -212,21 +194,18 @@ sub reindex {
     unless $self->pg->db->select('bot_packages', 'id',
     {id => $id, indexed => {'!=' => undef}, '-not_bool' => 'obsolete'})->hash;
 
-  $self->index($id, $priority);
+  $self->index($id, @args);
 
   return 1;
 }
 
-sub reindex_all {
-  my $self = shift;
-  my $ids  = $self->pg->db->query('select id from bot_packages where obsolete is not true')->arrays->flatten->to_array;
+sub reindex_all ($self) {
+  my $ids = $self->pg->db->query('select id from bot_packages where obsolete is not true')->arrays->flatten->to_array;
   my $minion = $self->minion;
   $minion->enqueue(index_later => [$_] => {priority => 0}) for @$ids;
 }
 
-sub reindex_matched_packages {
-  my ($self, $pid, $priority) = (shift, shift, shift // 0);
-
+sub reindex_matched_packages ($self, $pid, $priority = 0) {
   my $package = $self->pg->db->query('select distinct package from pattern_matches where pattern = ?', $pid);
   my $minion  = $self->minion;
   for my $row ($package->hashes->each) {
@@ -234,42 +213,36 @@ sub reindex_matched_packages {
   }
 }
 
-sub states {
-  my ($self, $name) = @_;
+sub states ($self, $name) {
   return $self->pg->db->query(
     'select checkout_dir as checkout, state from bot_packages
      where name = ? order by created desc', $name
   )->hashes->to_array;
 }
 
-sub unpack { shift->_enqueue('unpack', @_) }
+sub unpack ($self, @args) { $self->_enqueue('unpack', @args) }
 
-sub unpacked {
-  my ($self, $id) = @_;
+sub unpacked ($self, $id) {
   $self->pg->db->update('bot_packages', {unpacked => \'now()'}, {id => $id});
 }
 
-sub update {
-  my ($self, $pkg) = @_;
+sub update ($self, $pkg) {
   my %updates = map { exists $pkg->{$_} ? ($_ => $pkg->{$_}) : () }
     (qw(created checksum priority state obsolete result), qw(reviewing_user external_link));
   $updates{reviewed} = \'now()' if $pkg->{review_timestamp};
   return $self->pg->db->update('bot_packages', \%updates, {id => $pkg->{id}});
 }
 
-sub keyword_files {
-  my ($self, $id) = @_;
+sub keyword_files ($self, $id) {
   return $self->pg->db->select('matched_files', 'id,filename', {package => $id})->hashes;
 }
 
-sub _check_timestamp {
-  my ($self, $field, $id) = @_;
+sub _check_timestamp ($self, $field, $id) {
   return undef unless my $hash = $self->pg->db->select('bot_packages', [$field], {id => $id})->hash;
   return !!$hash->{$field};
 }
 
-sub _enqueue {
-  my ($self, $task, $id, $priority, $parents, $delay) = (shift, shift, shift, shift // 5, shift || [], shift // 0);
+sub _enqueue ($self, $task, $id, $priority = 5, $parents = [], $delay = 0) {
   my $pkg = $self->find($id);
   return $self->minion->enqueue(
     $task => [$id] => {
