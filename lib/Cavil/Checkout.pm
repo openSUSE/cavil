efa58ffb3003dc45e63cf3384f5ba5e3ce86c912
Sebastian Riedel <s@kraih.com>
2024-04-23 19:33:15 +0200

    Increase maximum file size for chromium

--- lib/Cavil/Checkout.pm
+++ lib/Cavil/Checkout.pm
@@ -217,10 +217,10 @@ sub unpack ($self, $options = {}) {
   my $u = File::Unpack2->new(
     verbose => 0,
 
-    # chromium's tar is 5.4GB (uncompressed, as file::unpack2
+    # chromium's tar is 18GB (uncompressed, as file::unpack2
     # first xz -cd before extracting tar, we need to need that
     # much. And reserve some space for future growth)
-    maxfilesize          => '7G',
+    maxfilesize          => '20G',
     one_shot             => 0,
     no_op                => 0,
     world_readable       => 1,
