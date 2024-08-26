978c3ad1149a0820b2855de5be34a65daf0ac6a9
Sebastian Riedel <s@kraih.com>
2024-08-26 19:50:35 +0200

    Shorten help text

--- assets/vue/EditSnippet.vue
+++ assets/vue/EditSnippet.vue
@@ -34,13 +34,13 @@
             rows="20"
           ></textarea>
           <div id="patternHelp" class="form-text">
-            Keyword matches are highlighted in <span class="keyword-line">red</span> and should be included in the
-            license pattern. Overlapping matches of exisitng license patterns are highlighted in
-            <span class="license-line">green</span> and safe to ignore.
-            <span v-if="hasAdminRole === true"> Click on the line number to see the matching pattern.</span> Use
-            expressions like <code>$SKIP5</code> to skip up to a certain number of words in your pattern. The expression
+            Keyword matches are <span class="keyword-line">red</span> and should be included in the license pattern.
+            Overlapping matches of exisitng license patterns are <span class="license-line">green</span> and safe to
+            ignore. Use expressions like <code>$SKIP5</code> to skip up to a certain number of words.
             <code>$SKIP19</code> represents the upper limit of the matching engine (which will be higher than 19), and
-            simply skips as many words as possible.
+            simply skips as many words as possible.<span v-if="hasAdminRole === true">
+              Click on line numbers to see patterns.</span
+            >
           </div>
         </div>
       </div>
